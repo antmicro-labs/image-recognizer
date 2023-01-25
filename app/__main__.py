65a8569f649d0b337a82f0a79b1374d1c7da0011
Michal Rojewski <michal@pop-os.localdomain>
2023-01-25 16:12:26 +0100

    cli all info at once

--- app/__main__.py
+++ app/__main__.py
@@ -2,5 +2,14 @@ from image_recognizer import ImageRecognizer
 
 import fire
 
+def rec(path):
+    imgRec = ImageRecognizer()
+    data = imgRec.recognize(path)
+    return { "format" : data.color_format, 
+             "format_condifence" : data.color_format_confidence,
+             "img_height" : data.img_height,
+             "img_width" : data.img_width,
+             "resolution_confidence" : data.resolution_confidence}
+
 if __name__ == "__main__":
-    fire.Fire(ImageRecognizer())
+    fire.Fire(rec)
--- run_examples.sh
+++ run_examples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ./.venv/bin/activate
 
-python app recognize tests/test_data/abgr444/picture_nr_5_640x480_abgr444.raw color_format_confidence
-python app recognize tests/test_data/RGB24/picture_nr_1_640x427.raw color_format
-python app recognize tests/test_data/abgr444/picture_nr_5_640x480_abgr444.raw img_width
+python app tests/test_data/abgr444/picture_nr_5_640x480_abgr444.raw
+python app tests/test_data/RGB24/picture_nr_1_640x427.raw
+python app tests/test_data/abgr444/picture_nr_5_640x480_abgr444.raw
